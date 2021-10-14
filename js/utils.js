b4598a6224ac295fc81a113c05eca6b7560960f0
Holder19 <Manhunt19@inbox.ru>
2021-10-14 21:41:46 +0300

    Выполняет разделение кода на модули

---
+++ js/utils.js
@@ -0,0 +1,21 @@
+const getRandomPositiveFloat = (min, max, digits = 1) => {
+  const lower = Math.min(Math.abs(min), Math.abs(max));
+  const upper = Math.max(Math.abs(min), Math.abs(max));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
+};
+
+const getRandomPositiveInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+const createAvatarNumber = (index) => index.toString().padStart(2, 0);
+
+const getArrayRandomLength = (array) => array.slice(getRandomPositiveInteger(1, array.length - 1));
+
+export {getRandomPositiveFloat, getRandomPositiveInteger, getRandomArrayElement, createAvatarNumber, getArrayRandomLength};
